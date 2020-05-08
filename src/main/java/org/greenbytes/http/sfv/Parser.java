4edbd4a210375096fbe5f823d87e8f89aa42db02
Julian Reschke <jreschke@adobe.com>
2020-05-08 16:01:45 +0100

    switch to CharBuffer

--- src/main/java/org/greenbytes/http/sfv/Parser.java
+++ src/main/java/org/greenbytes/http/sfv/Parser.java
@@ -1,31 +1,30 @@
 package org.greenbytes.http.sfv;
 
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
 
-    public static Item parseIntegerOrDecimal(StringBuilder input) {
+    public static Item parseIntegerOrDecimal(CharBuffer input) {
         String type = "integer";
         int sign = 1;
-        int consumed = 0;
         StringBuilder inputNumber = new StringBuilder(20);
 
-        if (input.length() > 0 && input.charAt(consumed) == '-') {
+        if (input.hasRemaining() && input.charAt(0) == '-') {
             sign = -1;
-            consumed += 1;
+            input.position(1 + input.position());
         }
 
-        if (input.length() == consumed) {
+        if (!input.hasRemaining()) {
             throw new IllegalArgumentException("empty Integer or Decimal");
+        } else if (!isDigit(input.charAt(0))) {
+            throw new IllegalArgumentException("illegal start character for Integer or Decimal: '" + input.charAt(0) + "'");
         }
 
-        if (!isDigit(input.charAt(consumed))) {
-            throw new IllegalArgumentException("illegal start character for Integer or Decimal: '" + input.charAt(consumed) + "'");
-        }
-
-        while (input.length() != consumed) {
-            char c = input.charAt(consumed++);
+        while (input.hasRemaining()) {
+            input.mark();
+            char c = input.get();
             if (isDigit(c)) {
                 inputNumber.append(c);
             } else if ("integer".equals(type) && c == '.') {
@@ -35,15 +34,13 @@ public class Parser {
                 inputNumber.append(c);
                 type = "decimal";
             } else {
-                consumed -= 1;
+                input.reset();
                 break;
             }
             if (inputNumber.length() > ("integer".equals(type) ? 15 : 16)) {
                 throw new IllegalArgumentException(type + " too long: " + inputNumber.length());
             }
         }
-
-        input.delete(0, consumed);
 
         if ("integer".equals(type)) {
             long l = Long.parseLong(inputNumber.toString());
@@ -70,12 +67,12 @@ public class Parser {
     }
 
     public static IntegerItem parseInteger(String input) {
-        StringBuilder sb = new StringBuilder(input);
+        CharBuffer sb = CharBuffer.wrap(input);
         Item result = parseIntegerOrDecimal(sb);
         if (!(result instanceof IntegerItem)) {
             throw new IllegalArgumentException("string parsed as integer '" + input + "' is a Decimal");
         } else {
-            if (sb.length() != 0) {
+            if (sb.hasRemaining()) {
                 throw new IllegalArgumentException("extra characters in string parsed as integer: '" + sb + "'");
             }
             return (IntegerItem) result;
@@ -83,40 +80,38 @@ public class Parser {
     }
 
     public static DecimalItem parseDecimal(String input) {
-        StringBuilder sb = new StringBuilder(input);
-        Item result = parseIntegerOrDecimal(sb);
+        CharBuffer buffer = CharBuffer.wrap(input);
+        Item result = parseIntegerOrDecimal(buffer);
         if (!(result instanceof DecimalItem)) {
             throw new IllegalArgumentException("string parsed as integer '" + input + "' is an Integer");
         } else {
-            if (sb.length() != 0) {
-                throw new IllegalArgumentException("extra characters in string parsed as decimal: '" + sb + "'");
+            if (buffer.hasRemaining()) {
+                throw new IllegalArgumentException("extra characters in string parsed as decimal: '" + buffer + "'");
             }
             return (DecimalItem) result;
         }
     }
 
-    private static Item parseString(StringBuilder inputString) {
-        int consumed = 0;
+    private static Item parseString(CharBuffer inputString) {
         StringBuilder outputString = new StringBuilder(inputString.length());
 
-        if (inputString.length() > 0 && inputString.charAt(consumed++) != '"') {
+        if (!inputString.hasRemaining() || inputString.get() != '"') {
             throw new IllegalArgumentException("must start with double quote");
         }
 
-        while (inputString.length() != consumed) {
-            char c = inputString.charAt(consumed++);
+        while (inputString.hasRemaining()) {
+            char c = inputString.get();
             if (c == '\\') {
-                if (inputString.length() == consumed) {
-                    throw new IllegalArgumentException("incomplete escape sequence at " + inputString.length());
+                if (!inputString.hasRemaining()) {
+                    throw new IllegalArgumentException("incomplete escape sequence at " + inputString.position());
                 }
-                c = inputString.charAt(consumed++);
+                c = inputString.get();
                 if (c != '"' && c != '\\') {
-                    throw new IllegalArgumentException("invalid escape sequence at " + inputString.length());
+                    throw new IllegalArgumentException("invalid escape sequence at " + inputString.position());
                 }
                 outputString.append(c);
             } else {
                 if (c == '"') {
-                    inputString.delete(0, consumed);
                     return new StringItem(outputString.toString());
                 } else if (c < 0x20 || c >= 0x7f) {
                     throw new IllegalArgumentException("invalid character at " + inputString.length());
@@ -130,44 +125,43 @@ public class Parser {
     }
 
     public static StringItem parseString(String input) {
-        StringBuilder sb = new StringBuilder(input);
-        Item result = parseString(sb);
-        if (sb.length() != 0) {
-            throw new IllegalArgumentException("extra characters in string parsed as integer: '" + sb + "'");
+        CharBuffer buffer = CharBuffer.wrap(input);
+        Item result = parseString(buffer);
+        if (buffer.length() != 0) {
+            throw new IllegalArgumentException("extra characters in string parsed as integer: '" + buffer + "'");
         }
         return (StringItem) result;
     }
 
-    private static Item parseBareItem(StringBuilder sb) {
-        if (sb.length() == 0) {
+    private static Item parseBareItem(CharBuffer buffer) {
+        if (!buffer.hasRemaining()) {
             throw new IllegalArgumentException("empty string");
         }
 
-        char c = sb.charAt(0);
+        char c = buffer.charAt(0);
         if (isDigit(c) || c == '-') {
-            return parseIntegerOrDecimal(sb);
+            return parseIntegerOrDecimal(buffer);
         } else if (c == '"') {
-            return parseString(sb);
+            return parseString(buffer);
         } else {
-            throw new IllegalArgumentException("unknown type: " + sb);
+            throw new IllegalArgumentException("unknown type: " + buffer);
         }
     }
 
-    private static List<Item> parseList(StringBuilder sb) {
+    private static List<Item> parseList(CharBuffer sb) {
         List<Item> result = new ArrayList<>();
 
-        while (sb.length() != 0) {
+        while (sb.hasRemaining()) {
             result.add(parseBareItem(sb));
             removeLeadingSP(sb);
-            if (sb.length() == 0) {
+            if (!sb.hasRemaining()) {
                 return result;
             }
-            if (sb.charAt(0) != ',') {
+            if (sb.get() != ',') {
                 throw new IllegalArgumentException("expected COMMA, got: " + sb);
             }
-            sb.deleteCharAt(0);
             removeLeadingSP(sb);
-            if (sb.length() == 0) {
+            if (!sb.hasRemaining()) {
                 throw new IllegalArgumentException("found trailing COMMA in list");
             }
         }
@@ -177,17 +171,17 @@ public class Parser {
     }
 
     public static ListItem parseList(String input) {
-        StringBuilder sb = new StringBuilder(input);
-        List<Item> result = parseList(sb);
-        if (sb.length() != 0) {
-            throw new IllegalArgumentException("extra characters in string parsed as list: '" + sb + "'");
+        CharBuffer buffer = CharBuffer.wrap(input);
+        List<Item> result = parseList(buffer);
+        if (buffer.hasRemaining()) {
+            throw new IllegalArgumentException("extra characters in string parsed as list: '" + buffer + "'");
         }
         return new ListItem(result);
     }
 
-    private static void removeLeadingSP(StringBuilder sb) {
-        while (sb.length() != 0 && sb.charAt(0) == ' ') {
-            sb.deleteCharAt(0);
+    private static void removeLeadingSP(CharBuffer sb) {
+        while (sb.hasRemaining() && sb.charAt(0) == ' ') {
+            sb.position(1 + sb.position());
         }
     }
 
