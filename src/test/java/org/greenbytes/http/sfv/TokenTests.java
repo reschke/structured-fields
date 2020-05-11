944a0be420e9b7ea1e7e6bb3590ced7dd7e431e0
Julian Reschke <julian.reschke@gmx.de>
2020-05-11 15:17:57 +0100

    token

---
+++ src/test/java/org/greenbytes/http/sfv/TokenTests.java
@@ -0,0 +1,68 @@
+package org.greenbytes.http.sfv;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class TokenTests extends AbstractSpecificationTests {
+
+    private TestParams p;
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> parameters() {
+        return AbstractSpecificationTests.makeParameters("token.json");
+    }
+
+    public TokenTests(Object x, Object y) {
+        this.p = (TestParams) y;
+    }
+
+    private Item<? extends Object> parse() {
+        if (p.header_type.equals("item")) {
+            return Parser.parseItem(p.raw);
+        } else if (p.header_type.equals("list")) {
+            return Parser.parseList(p.raw);
+        } else {
+            fail("unsupported header type");
+            return null;
+        }
+    }
+
+    @Test
+    public void runTest() {
+        if (p.must_fail) {
+            try {
+                parse();
+                fail("should fail");
+            } catch (IllegalArgumentException expected) {
+            }
+        } else {
+            Item<? extends Object> item = parse();
+            if (item instanceof ListItem) {
+                List<Item<? extends Object>> list = ((ListItem) item).get();
+                JsonObject expected = (JsonObject) ((JsonArray) p.expected_value).get(0);
+                CharSequence expectedString = ((JsonString) expected.get("value")).getChars();
+                assertEquals(expectedString, list.get(0).get());
+            } else {
+                JsonObject expected = (JsonObject) p.expected_value;
+                CharSequence expectedString = ((JsonString) expected.get("value")).getChars();
+                assertEquals(expectedString, item.get());
+            }
+
+            if (p.canonical != null) {
+                assertEquals(p.canonical, item.serialize());
+            }
+        }
+    }
+}
