12165e18fe4aa1ddc27a6d6b38a653f3af088255
Julian Reschke <jreschke@adobe.com>
2020-05-12 15:52:08 +0100

    string-generated

--- src/test/java/org/greenbytes/http/sfv/SpecificationTests.java
+++ src/test/java/org/greenbytes/http/sfv/SpecificationTests.java
@@ -12,10 +12,10 @@ public class SpecificationTests extends AbstractSpecificationTests {
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> parameters() {
-        return AbstractSpecificationTests
-                .makeParameters(Arrays.asList(new String[] { "binary.json", "boolean.json", "dictionary.json", "examples.json",
-                        "item.json", "key-generated.json", "large-generated.json", "list.json", "listlist.json", "number.json",
-                        "number-generated.json", "param-dict.json", "param-list.json", "string.json", "token.json" }));
+        return AbstractSpecificationTests.makeParameters(Arrays.asList(
+                new String[] { "binary.json", "boolean.json", "dictionary.json", "examples.json", "item.json", "key-generated.json",
+                        "large-generated.json", "list.json", "listlist.json", "number.json", "number-generated.json",
+                        "param-dict.json", "param-list.json", "string.json", "string-generated.json", "token.json" }));
     }
 
     public SpecificationTests(Object x, Object y) {
