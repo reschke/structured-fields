c5e2f490dc3eab7cea4b3d61da8c41863bb0e0a7
Julian Reschke <julian.reschke@gmx.de>
2020-05-13 18:21:08 +0100

    start javadoc

---
+++ src/main/java/org/greenbytes/http/sfv/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Implementation of IETF draft <a href=
+ * "https://greenbytes.de/tech/webdav/draft-ietf-httpbis-header-structure-18.html">Structured
+ * Field Values for HTTP</a>.
+ * <p>
+ * Includes a {@link Parser} and object equivalents of the defined data types
+ * (see {@link Item}).
+ */
+
+package org.greenbytes.http.sfv;
