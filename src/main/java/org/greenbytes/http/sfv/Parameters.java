b7b475454e1171aee02042bb91d7686bdb9c563b
Julian Reschke <julian.reschke@gmx.de>
2020-05-14 13:55:14 +0100

    Parameters cleanup & javadoc

--- src/main/java/org/greenbytes/http/sfv/Parameters.java
+++ src/main/java/org/greenbytes/http/sfv/Parameters.java
@@ -3,9 +3,15 @@ package org.greenbytes.http.sfv;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Supplier;
 
-public class Parameters implements Supplier<Map<String, Item<? extends Object>>> {
+/**
+ * Represents the Parameters of an Item or an Inner List.
+ * 
+ * @see <a href=
+ *      "https://greenbytes.de/tech/webdav/draft-ietf-httpbis-header-structure-18.html#param">Section
+ *      3.1.2 of draft-ietf-httpbis-header-structure-18</a>
+ */
+public class Parameters implements Type<Map<String, Item<? extends Object>>> {
 
     private final Map<String, Item<? extends Object>> value;
 
@@ -16,6 +22,18 @@ public class Parameters implements Supplier<Map<String, Item<? extends Object>>>
         checkKeys(this.value);
     }
 
+    /**
+     * Creates a {@link Parameters} instance representing the specified
+     * {@code Map<String, Item>} value.
+     * <p>
+     * Note that the {@link Map} implementation that is used here needs to
+     * iterate predictably based on insertion order, such as
+     * {@link java.util.LinkedHashMap}.
+     * 
+     * @param value
+     *            a {@code Map<String, Item>} value
+     * @return a {@link Parameters} representing {@code value}.
+     */
     public static Parameters valueOf(Map<String, Item<? extends Object>> value) {
         return new Parameters(value);
     }
@@ -25,6 +43,7 @@ public class Parameters implements Supplier<Map<String, Item<? extends Object>>>
         return value;
     }
 
+    @Override
     public StringBuilder serializeTo(StringBuilder sb) {
         for (Map.Entry<String, Item<? extends Object>> e : value.entrySet()) {
             sb.append(';').append(e.getKey());
@@ -36,6 +55,7 @@ public class Parameters implements Supplier<Map<String, Item<? extends Object>>>
         return sb;
     }
 
+    @Override
     public String serialize() {
         return serializeTo(new StringBuilder()).toString();
     }
