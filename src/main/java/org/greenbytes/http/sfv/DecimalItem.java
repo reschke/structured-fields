8f95e55768eeeb7b2b95c5ffc6caded25a893a21
Julian Reschke <julian.reschke@gmx.de>
2020-05-14 12:56:45 +0100

    Decimal cleanup & javadoc

--- src/main/java/org/greenbytes/http/sfv/DecimalItem.java
+++ src/main/java/org/greenbytes/http/sfv/DecimalItem.java
@@ -3,6 +3,22 @@ package org.greenbytes.http.sfv;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+/**
+ * Represents a Decimal.
+ * <p>
+ * A Decimal - despite it's name - is essentially the same thing as an Integer,
+ * but has an implied divisor of 1000 (in other words, a scale of 3). Thus, a
+ * value represented as {@code 0.5} in a field value will be internally stored
+ * as {@code long} with value {@code 500}. The only difference to
+ * {@link IntegerItem} is that {@link #get()} will return a {@link BigDecimal},
+ * and that the implied divisor is taken into account when serializing the
+ * value. {@link #getAsLong()} provides access to the raw value when the
+ * overhead of {@link BigDecimal} is not needed.
+ * 
+ * @see <a href=
+ *      "https://greenbytes.de/tech/webdav/draft-ietf-httpbis-header-structure-18.html#decimal">Section
+ *      3.3.2 of draft-ietf-httpbis-header-structure-18</a>
+ */
 public class DecimalItem implements NumberItem<BigDecimal> {
 
     private final long value;
@@ -11,7 +27,7 @@ public class DecimalItem implements NumberItem<BigDecimal> {
     private static final long MIN = -999999999999999L;
     private static final long MAX = 999999999999999L;
 
-    public DecimalItem(long value, Parameters params) {
+    private DecimalItem(long value, Parameters params) {
         if (value < MIN || value > MAX) {
             throw new IllegalArgumentException("value must be in the range from " + MIN + " to " + MAX);
         }
@@ -19,6 +35,14 @@ public class DecimalItem implements NumberItem<BigDecimal> {
         this.params = Objects.requireNonNull(params, "params must not be null");
     }
 
+    /**
+     * Creates a {@link DecimalItem} representing the specified {@code long}
+     * value, where the implied divisor is {@code 1000}.
+     * 
+     * @param value
+     *            a {@code long} value.
+     * @return a {@link DecimalItem} representing {@code value}.
+     */
     public static DecimalItem valueOf(long value) {
         return new DecimalItem(value, Parameters.EMPTY);
     }
