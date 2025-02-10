da4f0ff0c51a6b157bb86f96a6d59599827d0229
Julian Reschke <julian.reschke@gmx.de>
2025-02-10 18:27:53 +0100

    start tests for static methods on Parser

--- src/test/java/org/greenbytes/http/sfv/ParserAPITests.java
+++ src/test/java/org/greenbytes/http/sfv/ParserAPITests.java
@@ -1,4 +1,34 @@
 package org.greenbytes.http.sfv;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class ParserAPITests {
+
+    @Test
+    public void testItemEmpty() {
+        expectParseException("", 0, "  ^ Empty string found when parsing Bare Item");
+    }
+
+    @Test
+    public void testItemNull() {
+        expectParseException("", 0, "  ^ Empty string found when parsing Bare Item");
+    }
+
+    @Test
+    public void testBadString() {
+        expectParseException("a b", 2, "  --^ (0x62) Extra characters in string parsed as Item");
+    }
+
+    private static void expectParseException(String input, int position, String diagostics) {
+        try {
+            Parser.parseItem(input);
+            fail("should trow");
+        } catch (ParseException pex) {
+            assertEquals(position, pex.getPosition());
+            assertEquals(">>" + input + "<<\n" + diagostics + "\n", pex.getDiagnostics());
+        }
+    }
 }
