1bcb8448c25cc77d5ca655595729226fd0bc857f
Julian Reschke <julian.reschke@gmx.de>
2025-02-27 16:21:03 +0100

    test static methods on Parser

--- src/test/java/org/greenbytes/http/sfv/ParserAPITests.java
+++ src/test/java/org/greenbytes/http/sfv/ParserAPITests.java
@@ -9,20 +9,44 @@ public class ParserAPITests {
 
     @Test
     public void testItemEmpty() {
-        expectParseException("", 0, "  ^ Empty string found when parsing Bare Item");
+        expectParseItemException("", 0, "  ^ Empty string found when parsing Bare Item");
     }
 
     @Test
     public void testItemNull() {
-        expectParseException("", 0, "  ^ Empty string found when parsing Bare Item");
+        expectParseItemException("", 0, "  ^ Empty string found when parsing Bare Item");
     }
 
     @Test
-    public void testBadString() {
-        expectParseException("a b", 2, "  --^ (0x62) Extra characters in string parsed as Item");
+    public void testItemBadString() {
+        expectParseItemException("a b", 2, "  --^ (0x62) Extra characters in string parsed as Item");
     }
 
-    private static void expectParseException(String input, int position, String diagostics) {
+    @Test
+    public void testItemBadDisplayString() {
+        expectParseItemException("%\"%80\"", 2, "  --^ (0x25) Invalid UTF-8 sequence (Input length = 1) before position 2");
+    }
+
+    @Test
+    public void testItemBadDisplayString2() {
+        expectParseItemException("%\"%20", 5, "  -----^ Closing DQUOTE missing");
+    }
+
+    @Test
+    public void testItemString() {
+        Item<?> parsed =  Parser.parseItem("\"123\"");
+        assertEquals(StringItem.class, parsed.getClass());
+        assertEquals("123", parsed.get());
+    }
+
+    @Test
+    public void testItemDisplayString() {
+        Item<?> parsed = Parser.parseItem("%\"%e2%82%ac%20rates\"");
+        assertEquals(DisplayStringItem.class, parsed.getClass());
+        assertEquals("\u20ac rates", parsed.get());
+    }
+
+    private static void expectParseItemException(String input, int position, String diagostics) {
         try {
             Parser.parseItem(input);
             fail("should trow");
