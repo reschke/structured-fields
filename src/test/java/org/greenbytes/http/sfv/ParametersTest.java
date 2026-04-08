417ea05adb4f3c64d14ff4506b5fff09e84f5cf3
Julian Reschke <julian.reschke@gmx.de>
2026-04-08 20:38:13 +0100

    equals() and hashCode() for items (wip)

--- src/test/java/org/greenbytes/http/sfv/ParametersTest.java
+++ src/test/java/org/greenbytes/http/sfv/ParametersTest.java
@@ -2,7 +2,14 @@ package org.greenbytes.http.sfv;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
 public class ParametersTest {
 
@@ -53,5 +60,25 @@ public class ParametersTest {
     @Test(expected =  UnsupportedOperationException.class)
     public void testReplaceAll1() {
         params.replaceAll(null);
+    }
+
+
+    @Test
+    public void testParametersEquality() {
+        Map m1 = new HashMap();
+        m1.put("a", BooleanItem.valueOf(true));
+        Map m2 = new HashMap();
+        m2.put("b", IntegerItem.valueOf(12));
+        m2.put("c", StringItem.valueOf("hello"));
+
+        Parameters p1 = Parameters.valueOf(m1);
+        Parameters p2 = Parameters.valueOf(m1);
+        Parameters p3 = Parameters.valueOf(m2);
+
+        assertFalse(p1 == p2);
+        assertEquals(p1, p2);
+
+        assertFalse(p1 == p3);
+        assertNotEquals(p1, p3);
     }
 }
