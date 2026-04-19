b398794547382370d8c48ad397f9dacf57a72fb1
Julian Reschke <julian.reschke@gmx.de>
2026-04-19 14:32:39 +0100

    StringItem

--- src/main/java/org/greenbytes/http/sfv/StringItem.java
+++ src/main/java/org/greenbytes/http/sfv/StringItem.java
@@ -60,8 +60,10 @@ public class StringItem implements Item<String> {
     public StringBuilder serializeToForDebug(StringBuilder sb, int indentLevel, Function<Class, String> formatter) {
         String indent = indentLevel != 0 ? String.format("%" + indentLevel + "s", "") : "";
         String classn = formatter.apply(this.getClass());
-        return sb.append(indent).append(value).append(classn).append("\n")
-                .append(params.serializeToForDebug(new StringBuilder(), indentLevel + 2, formatter)).append("\n");
+
+        sb = sb.append(indent).append(value).append(classn).append("\n");
+        sb = params.serializeToForDebug(sb, indentLevel + 2, formatter);
+        return sb;
     }
 
     @Override
