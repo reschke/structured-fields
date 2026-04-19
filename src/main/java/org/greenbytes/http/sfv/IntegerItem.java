9a6a48c59998c06f8ce07ca842385386c6a19b52
Julian Reschke <julian.reschke@gmx.de>
2026-04-19 14:29:33 +0100

    IntegerItem

--- src/main/java/org/greenbytes/http/sfv/IntegerItem.java
+++ src/main/java/org/greenbytes/http/sfv/IntegerItem.java
@@ -63,8 +63,10 @@ public class IntegerItem implements NumberItem<Long> {
     public StringBuilder serializeToForDebug(StringBuilder sb, int indentLevel, Function<Class, String> formatter) {
         String indent = indentLevel != 0 ? String.format("%" + indentLevel + "s", "") : "";
         String classn = formatter.apply(this.getClass());
-        return sb.append(indent).append(value).append(classn).append("\n")
-                .append(params.serializeToForDebug(new StringBuilder(), indentLevel + 2, formatter));
+
+        sb = sb.append(indent).append(value).append(classn).append("\n");
+        sb = params.serializeToForDebug(sb, indentLevel + 2, formatter);
+        return sb;
     }
 
     @Override
