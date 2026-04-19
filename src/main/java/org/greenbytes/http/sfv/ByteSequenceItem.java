b6a0c3677ca36c9a019102a4b188cf2c0e6387ba
Julian Reschke <julian.reschke@gmx.de>
2026-04-19 14:18:31 +0100

    ByteSequenceItem

--- src/main/java/org/greenbytes/http/sfv/ByteSequenceItem.java
+++ src/main/java/org/greenbytes/http/sfv/ByteSequenceItem.java
@@ -67,8 +67,12 @@ public class ByteSequenceItem implements Item<ByteBuffer> {
     public StringBuilder serializeToForDebug(StringBuilder sb, int indentLevel, Function<Class, String> formatter) {
         String indent = indentLevel != 0 ? String.format("%" + indentLevel + "s", "") : "";
         String classn = formatter.apply(this.getClass());
-        return sb.append(indent).append(serializeToNoParams(new StringBuilder())).append(classn).append("\n")
-                .append(params.serializeToForDebug(new StringBuilder(), indentLevel + 2, formatter)).append("\n");
+
+        sb = sb.append(indent);
+        sb = serializeToNoParams(sb);
+        sb.append(classn).append("\n");
+        sb = params.serializeToForDebug(sb, indentLevel + 2, formatter);
+        return sb;
     }
 
 
